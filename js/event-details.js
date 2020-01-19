362e691ea337b34bf6e5df3c0179ce0bec6726b5
Sarthak Mittal <sam.mittal.2106@gmail.com>
2020-01-19 22:02:03 +0530

    refactoring

--- js/event-details.js
+++ js/event-details.js
@@ -29,8 +29,8 @@ $(document).ready(function() {
   // Activate the image for the navbar-collapse
   blackKit.initNavbarImage();
 
-  $navbar = $('.navbar[color-on-scroll]');
-  scroll_distance = $navbar.attr('color-on-scroll') || 500;
+  navbar = $('.navbar[color-on-scroll]');
+  scroll_distance = navbar.attr('color-on-scroll') || 500;
 
   // Check if we have the class "navbar-color-on-scroll" then add the function to remove the class "navbar-transparent" so it will transform to a plain color.
 
