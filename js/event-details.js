72ab87032df8e267fd82518d3c5bede7b848ea90
Sarthak Mittal <sam.mittal.2106@gmail.com>
2020-01-23 14:53:50 +0530

    navbar fix

--- js/event-details.js
+++ js/event-details.js
@@ -13,6 +13,27 @@ var $datepicker = $('.datepicker');
 var $collapse = $('.navbar .collapse');
 var $html = $('html');
 
+(function() {
+  var isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+
+  if (isWindows) {
+    // if we are on windows OS we activate the perfectScrollbar function
+
+
+    if ($('.tab-content .table-responsive').length != 0) {
+
+      $('.table-responsive').each(function() {
+        var ps2 = new PerfectScrollbar($(this)[0]);
+      });
+    }
+
+
+
+    $html.addClass('perfect-scrollbar-on');
+  } else {
+    $html.addClass('perfect-scrollbar-off');
+  }
+})();
 
 $(document).ready(function() {
   //  Activate the Tooltips
@@ -26,11 +47,38 @@ $(document).ready(function() {
     });
   });
 
+  var squares1 = document.getElementById("square1");
+  var squares2 = document.getElementById("square2");
+  var squares3 = document.getElementById("square3");
+  var squares4 = document.getElementById("square4");
+  var squares5 = document.getElementById("square5");
+  var squares6 = document.getElementById("square6");
+  var squares9 = document.getElementById("square7");
+  var squares10 = document.getElementById("square8");
+
+  if ($('.square').length != 0) {
+
+    $(document).mousemove(function(e) {
+      posX = event.clientX - window.innerWidth / 2;
+      posY = event.clientY - window.innerWidth / 6;
+
+      squares1.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+      squares2.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+      squares3.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+      squares4.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+      squares5.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+      squares6.style.transform = "perspective(500px) rotateY(" + posX * 0.05 + "deg) rotateX(" + posY * (-0.05) + "deg)";
+      squares9.style.transform = "perspective(500px) rotateY(" + posX * 0.02 + "deg) rotateX(" + posY * (-0.02) + "deg)";
+      squares10.style.transform = "perspective(500px) rotateY(" + posX * 0.02 + "deg) rotateX(" + posY * (-0.02) + "deg)";
+
+    });
+  }
+
   // Activate the image for the navbar-collapse
   blackKit.initNavbarImage();
 
-  navbar = $('.navbar[color-on-scroll]');
-  scroll_distance = navbar.attr('color-on-scroll') || 500;
+  $navbar = $('.navbar[color-on-scroll]');
+  scroll_distance = $navbar.attr('color-on-scroll') || 500;
 
   // Check if we have the class "navbar-color-on-scroll" then add the function to remove the class "navbar-transparent" so it will transform to a plain color.
 
@@ -39,6 +87,11 @@ $(document).ready(function() {
     $(window).on('scroll', blackKit.checkScrollForTransparentNavbar)
   }
 
+  $('.form-control').on("focus", function() {
+    $(this).parent('.input-group').addClass("input-group-focus");
+  }).on("blur", function() {
+    $(this).parent(".input-group").removeClass("input-group-focus");
+  });
 
   // Activate bootstrapSwitch
   $('.bootstrap-switch').each(function() {
@@ -52,7 +105,10 @@ $(document).ready(function() {
     });
   });
 
-  
+  // Activate Carousel
+  $('.carousel').carousel({
+    interval: false
+  });
 });
 
 // Methods
@@ -83,6 +139,24 @@ if ($collapse.length) {
 }
 
 
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
+};
 
 $(document).on('click', '.navbar-toggler', function() {
   $toggle = $(this);
@@ -151,7 +225,72 @@ blackKit = {
     }
   },
 
+  initDatePicker: function() {
+    if ($datepicker.length != 0) {
+      $datepicker.datetimepicker({
+        icons: {
+          time: "tim-icons icon-watch-time",
+          date: "tim-icons icon-calendar-60",
+          up: "fa fa-chevron-up",
+          down: "fa fa-chevron-down",
+          previous: 'tim-icons icon-minimal-left',
+          next: 'tim-icons icon-minimal-right',
+          today: 'fa fa-screenshot',
+          clear: 'fa fa-trash',
+          close: 'fa fa-remove'
+        }
+      });
+    }
+  },
+
+  initSliders: function() {
+    // Sliders for demo purpose in refine cards section
+    var slider = document.getElementById('sliderRegular');
+    if ($('#sliderRegular').length != 0) {
+
+      noUiSlider.create(slider, {
+        start: 40,
+        connect: [true, false],
+        range: {
+          min: 0,
+          max: 100
+        }
+      });
+    }
+
+    var slider2 = document.getElementById('sliderDouble');
+
+    if ($('#sliderDouble').length != 0) {
+
+      noUiSlider.create(slider2, {
+        start: [20, 60],
+        connect: true,
+        range: {
+          min: 0,
+          max: 100
+        }
+      });
+    }
+  }
 }
 
 
 
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
+};
