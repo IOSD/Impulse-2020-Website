22c92214902228e746f2410a1429f06b16371cfd
roopamgarg <roopamg777@gmail.com>
2020-02-22 21:18:57 +0530

    theme changes

--- js/hackmait/countdown.js
+++ js/hackmait/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 15, 2020 00:00:00").getTime();
+var countDownDate = new Date("Mar 19, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
