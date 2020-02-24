344b9219e885e7f97ee128c9392a6daa6856c10b
roopamgarg <roopamg777@gmail.com>
2020-02-24 09:34:03 +0530

    added themes

--- js/hackmait/navbar.js
+++ js/hackmait/navbar.js
@@ -1,23 +1,24 @@
+window.addEventListener("mousewheel", event => {
+  let originalEvent = event.wheelDeltaY;
 
-window.addEventListener('mousewheel', (event)=> {
-
-    let originalEvent = event.wheelDeltaY;
-
-    if (event.wheelDeltaY >= 0) {
-        document.getElementById('navbar').style.opacity = 1
-
-    }
-    else {
-        document.getElementById('navbar').style.opacity = 0
-
-    }
+  if (event.wheelDeltaY >= 0) {
+    document.getElementById("navbar").style.opacity = 1;
+  } else {
+    document.getElementById("navbar").style.opacity = 0;
+  }
 });
-console.log(document.getElementById('nav-menu'))
-document.getElementById('nav-menu').addEventListener('click', () => {
-    document.getElementById('navs').style.display = "block"
-})
+console.log(document.getElementById("nav-menu"));
+document.getElementById("nav-menu").addEventListener("click", () => {
+  document.getElementById("navs").style.display = "block";
+ 
+});
 
-document.getElementById('navs').addEventListener('click',()=>{
-    document.getElementById('navs').style.display = "none"
-
-})
+document.getElementById("navs").addEventListener("click", () => {
+    const width =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+    if(width < 720){
+    document.getElementById("navs").style.display = "none";
+    }
+  });
