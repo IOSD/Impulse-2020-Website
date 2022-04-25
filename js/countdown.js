603f41dff73a5b33389f4c6af9b4e7f333ebc7b5
CoderGaurav01 <gauravchhapliyal2001@gmail.com>
2022-04-25 14:47:21 +0530

    nav page bug and countdown clock

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 30, 2020 09:00:00").getTime();
+var countDownDate = new Date("May 12, 2022 09:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
